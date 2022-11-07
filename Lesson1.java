1fa6b357eab7bb273eb2f0cd17475bec8f638f61
Igor Cebota <chebota2@gmail.com>
2022-11-07 18:47:56 +0200

    Промежуточный коммит.

--- Lesson1.java
+++ Lesson1.java
@@ -1,25 +1,20 @@
 import java.time.LocalDate;
-import java.util.Locale;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        LocalDate data = LocalDate.of(1997, 12, 2);
-        Person current = new Person("Игорь", data, "Мужской");
+        Person me = new Person("Igor", LocalDate.of(1997, 12, 2), "Мужской");
+        Person p1 = new Person("Женя", LocalDate.of(1998, 1, 1), "Мужской");
+        Person p2 = new Person("Маша", LocalDate.of(1999, 2, 2), "Женский");
+        Person p3 = new Person("Даша", LocalDate.of(2000, 3, 3), "Женский");
+        Person p4 = new Person("ГАААЛЯ", LocalDate.of(1970, 4, 4), "Женский");
+        Person p5 = new Person("Федя", LocalDate.of(1990, 5, 5), "Мужской");
+        Person p6 = new Person("Вася", LocalDate.of(1945, 9, 2), "Мужской");
 
-        System.out.println(current);
-
-        LocalDate dataM = LocalDate.of(1973, 12, 25);
-        LocalDate dataF = LocalDate.of(1965, 12, 4);
-
-        Person person1 = new Person("Александр", dataF, "Мужской");
-        Person person2 = new Person("Виктория", dataM, "Женский");
-
-        System.out.println(person1);
-        System.out.println(person2);
-
-        FamilyTreeElement el = new FamilyTreeElement(current);
-        el.addParent(person1);
-        el.addParent(person2);
-        System.out.println(el.getInfo());
+        me.addRelative(p1, Person.connection.Father);
+        p1.addRelative(p2, Person.connection.Mother);
+        p2.addRelative(p3, Person.connection.Mother);
+        p3.addRelative(p4, Person.connection.Mother);
+        p4.addRelative(p5, Person.connection.Father);
+        p5.addRelative(p6, Person.connection.Father);
     }
 }
