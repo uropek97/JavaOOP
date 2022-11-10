220ac4de9b7f60bb2c1d2f4bdd59e7a9ca0b8593
Igor Cebota <chebota2@gmail.com>
2022-11-10 16:33:29 +0200

    Промежуточный коммит.

--- Lesson1.java
+++ Lesson1.java
@@ -2,19 +2,45 @@ import java.time.LocalDate;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        Person me = new Person("Igor", LocalDate.of(1997, 12, 2), "Мужской");
-        Person p1 = new Person("Женя", LocalDate.of(1998, 1, 1), "Мужской");
-        Person p2 = new Person("Маша", LocalDate.of(1999, 2, 2), "Женский");
-        Person p3 = new Person("Даша", LocalDate.of(2000, 3, 3), "Женский");
-        Person p4 = new Person("ГАААЛЯ", LocalDate.of(1970, 4, 4), "Женский");
-        Person p5 = new Person("Федя", LocalDate.of(1990, 5, 5), "Мужской");
-        Person p6 = new Person("Вася", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p0 = new Person("0", LocalDate.of(1997, 12, 2), "Мужской");
+        Person p1 = new Person("1", LocalDate.of(1998, 1, 1), "Мужской");
+        Person p2 = new Person("2", LocalDate.of(1999, 2, 2), "Женский");
+        Person p3 = new Person("3", LocalDate.of(2000, 3, 3), "Женский");
+        Person p4 = new Person("4", LocalDate.of(1970, 4, 4), "Женский");
+        Person p5 = new Person("5", LocalDate.of(1990, 5, 5), "Мужской");
+        Person p6 = new Person("6", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p7 = new Person("7", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p8 = new Person("8", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p9 = new Person("9", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p10 = new Person("10", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p11 = new Person("11", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p12 = new Person("12", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p13 = new Person("13", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p14 = new Person("14", LocalDate.of(1945, 9, 2), "Мужской");
+        Person p15 = new Person("15", LocalDate.of(1945, 9, 2), "Мужской");
 
-        me.addRelative(p1, Person.connection.Father);
+
+        p0.addRelative(p1, Person.connection.Mother);
+        p0.addRelative(p7, Person.connection.Father);
         p1.addRelative(p2, Person.connection.Mother);
+        p1.addRelative(p8, Person.connection.Father);
         p2.addRelative(p3, Person.connection.Mother);
+        p2.addRelative(p9, Person.connection.Father);
         p3.addRelative(p4, Person.connection.Mother);
-        p4.addRelative(p5, Person.connection.Father);
-        p5.addRelative(p6, Person.connection.Father);
+        p3.addRelative(p10, Person.connection.Father);
+        p4.addRelative(p5, Person.connection.Mother);
+        p4.addRelative(p11, Person.connection.Father);
+        p5.addRelative(p6, Person.connection.Mother);
+        p5.addRelative(p12, Person.connection.Father);
+        p6.addRelative(p13, Person.connection.Mother);
+        p6.addRelative(p14, Person.connection.Father);
+        p7.addRelative(p15, Person.connection.Mother);
+
+        FamilyTree FT = new FamilyTree(p0);
+
+        System.out.println(FT.getInfoTree());
+
+        System.out.println("/_____________________/");
+        System.out.println(FT.getPerson());
     }
 }
