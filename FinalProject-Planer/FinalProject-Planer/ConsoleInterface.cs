a39bac0668f760eca81cd8ec8013c6a5113d827b
Igor Cebota <chebota2@gmail.com>
2022-12-24 13:52:47 +0200

    методы получения данных и информирования пользователя

--- FinalProject-Planer/FinalProject-Planer/ConsoleInterface.cs
+++ FinalProject-Planer/FinalProject-Planer/ConsoleInterface.cs
@@ -2,5 +2,18 @@
 {
     public class ConsoleInterface : IUserInterface
     {
+        public string getData(string message)
+        {
+            printMessage(message, false);
+            return Console.ReadLine()!;
+        }
+
+        public void printMessage(string message, bool ifNew)
+        {
+            if (ifNew)
+                Console.WriteLine(message);
+            else
+                Console.Write(message);
+        }
     }
 }
--- FinalProject-Planer/FinalProject-Planer/IUserInterface.cs
+++ FinalProject-Planer/FinalProject-Planer/IUserInterface.cs
@@ -2,5 +2,7 @@
 {
     public interface IUserInterface
     {
+        public void printMessage(string message, bool ifNew);//ifNew - с новой строки, или в отдельном окне
+        public string getData(string message);
     }
 }
