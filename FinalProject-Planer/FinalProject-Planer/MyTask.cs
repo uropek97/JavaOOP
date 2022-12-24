bf1c323d468bf89d67517d4764ed6d6790cb9e47
Igor Cebota <chebota2@gmail.com>
2022-12-24 13:47:55 +0200

    поля, свойства, конструкторы MyTask

--- FinalProject-Planer/FinalProject-Planer/MyTask.cs
+++ FinalProject-Planer/FinalProject-Planer/MyTask.cs
@@ -2,5 +2,25 @@
 {
     public class MyTask
     {
+        private string? name;
+        private DateTime creationDate;
+        private bool ifUrgent = false;
+
+        public string? Name { get { return name; } set { name = value; } }
+        public DateTime CreationDate { get { return creationDate; } set { creationDate = value; } }
+        public bool IfUrgent { get { return ifUrgent; } set { ifUrgent = value; } }
+        public MyTask()
+        {
+            this.CreationDate = DateTime.Now;
+        }
+        public MyTask(string? name) : this()
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return this.Name!.ToString();
+        }
     }
 }
