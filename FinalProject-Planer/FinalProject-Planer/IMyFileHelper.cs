c8a684fb743d25c38405e518c5f42787c6fa4c2f
Igor Cebota <chebota2@gmail.com>
2022-12-24 15:28:18 +0200

    работа с файлом. начало

---
+++ FinalProject-Planer/FinalProject-Planer/IMyFileHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProject_Planer
+{
+    public interface IMyFileHelper
+    {
+        public void Write(string data, string fileName);
+
+        public string Read(string fileName);
+    }
+}
---
+++ FinalProject-Planer/FinalProject-Planer/MyXMLFileHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace FinalProject_Planer
+{
+    public class MyXMLFileHelper : IMyFileHelper
+    {
+        private FileInfo file = new FileInfo("planer.txt");
+
+        public XmlSerializer? Xml = new XmlSerializer(typeof(Planer));
+        public string Read(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string data, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
