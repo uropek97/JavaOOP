abf227438f0323e48bb218bde66b33f6b586fd7a
Igor Cebota <chebota2@gmail.com>
2022-12-24 13:49:15 +0200

    поля, свойства, методы MyPurpose

--- FinalProject-Planer/FinalProject-Planer/MyPurpose.cs
+++ FinalProject-Planer/FinalProject-Planer/MyPurpose.cs
@@ -2,5 +2,33 @@
 {
     public class MyPurpose
     {
+        private string? name;
+        private List<MyTask>? tasks;
+
+        public string? Name { get { return name; } set { name = value; } }
+        public List<MyTask>? Tasks { get { return tasks; } set { tasks = value; } }
+
+        public MyPurpose() { }
+
+        public MyPurpose(string? name)
+        {
+            this.Name = name;
+            this.Tasks = new List<MyTask>();
+        }
+
+        public MyPurpose(string? name, MyTask myTask) : this(name)
+        {
+            this.Tasks!.Add(myTask);
+        }
+
+        public MyPurpose(string? name, List<MyTask> myTasks) : this(name)
+        {
+            this.Tasks!.AddRange(myTasks);
+        }
+
+        public override string ToString()
+        {
+            return this.Name!.ToString();
+        }
     }
 }
