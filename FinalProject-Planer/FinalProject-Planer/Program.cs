2a44f5eb0ce2c10f3c6232fdaf270d6d9310d224
Igor Cebota <chebota2@gmail.com>
2022-12-25 17:21:15 +0200

    добавление задач целям. завершение ДЗ

--- FinalProject-Planer/FinalProject-Planer/Program.cs
+++ FinalProject-Planer/FinalProject-Planer/Program.cs
@@ -1,4 +1,16 @@
 ﻿using FinalProject_Planer;
+using System.Xml.Serialization;
+
+if (!File.Exists("planet.xml"))
+{
+    File.Create("planet.xml");
+    Planer planer = new Planer();
+    XmlSerializer xml = new XmlSerializer(typeof(Planer));
+    using(var fs = new FileStream("planer.xml", FileMode.Open, FileAccess.Write))
+    {
+        xml.Serialize(fs, planer);
+    }
+}
 
 var presenter = new Presenter(new ConsoleInterface(), new MyXMLFileHelper());
 presenter.Work();
