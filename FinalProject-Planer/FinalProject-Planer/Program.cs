e577f695b4f67f05cad4c47bbb1994c6a1203a78
Igor Cebota <chebota2@gmail.com>
2022-12-25 17:32:59 +0200

    Небольшие исправления

--- FinalProject-Planer/FinalProject-Planer/Program.cs
+++ FinalProject-Planer/FinalProject-Planer/Program.cs
@@ -3,10 +3,9 @@ using System.Xml.Serialization;
 
 if (!File.Exists("planer.xml"))
 {
-    File.Create("planer.xml");
     Planer planer = new Planer();
     XmlSerializer xml = new XmlSerializer(typeof(Planer));
-    using(var fs = new FileStream("planer.xml", FileMode.Open, FileAccess.Write))
+    using(var fs = new FileStream("planer.xml", FileMode.OpenOrCreate, FileAccess.Write))
     {
         xml.Serialize(fs, planer);
     }
